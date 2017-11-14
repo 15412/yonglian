25c5028f97608b542f918844014e6ce9ccef7ebf
rohochan@gmail.com <rohochan>
2017-11-14 16:48:17 +0800

    init
    init

---
+++ Application/User/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+/**
+ * UCenter客户端配置文件
+ * 注意：该配置文件请使用常量方式定义
+ */
+
+define('UC_APP_ID', 1); //应用ID
+define('UC_API_TYPE', 'Model'); //可选值 Model / Service
+define('UC_AUTH_KEY', 'RTVn7r2yAzpN"l?%g-qi}9Yw1E0+>o3dxG|#j^h~'); //加密KEY
+define('UC_DB_DSN', 'mysql://root:@127.0.0.1:3306/yl'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', 'yl_'); // 数据表前缀，使用Model方式调用API必须配置此项
