845f9858b0ffae9ab1c42453c03e25433691cde4
zavier <zadvieryip@gmail.com>
2017-11-29 22:41:24 +0800

    添加网页模板文件 Signed-off-by: rohochan <rohochan@users.noreply.github.com>

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -14,9 +14,7 @@ namespace Home\Controller;
  * 前台首页控制器
  * 主要获取首页聚合数据
  */
-class IndexController extends HomeController
-{
-
+class IndexController extends HomeController{
     /**
      * 开始页
      */
@@ -30,11 +28,37 @@ class IndexController extends HomeController
     public function index(){
         //$category = D('Category')->getTree();
         //$lists    = D('Document')->lists(null);
-
         //$this->assign('category',$category);//栏目
         //$this->assign('lists',$lists);//列表
         //$this->assign('page',D('Document')->page);//分页
-
+        $this->display();
+    }
+    
+    /**
+     * 公司简介
+     */
+    public function introduction(){
+        $this->display();
+    }
+    
+    /**
+     * 	公司荣誉
+     */
+    public function honor(){
+        $this->display();
+    }
+    
+    /**
+     * 合作伙伴
+     */
+    public function cooperate(){
+        $this->display();
+    }
+    
+    /**
+     * 联系我们
+     */
+    public function contact(){
         $this->display();
     }
 
