d50a1dede1e4294028c11734b7e9f0460967aa68
rohochan <rohochan@users.noreply.github.com>
2017-12-31 22:25:28 +0800

    适配移动端
    Signed-off-by: rohochan <rohochan@users.noreply.github.com>

--- Application/Home/Lang/en-us.php
+++ Application/Home/Lang/en-us.php
@@ -1,9 +1,24 @@
 <?php
 return array(
-	'COMPANY_NAME'=>'YongLian',
+	'COMPANY_NAME'=>'WingLuen',
 	'MENU_INDEX'=>'Home',
 	'MENU_ABOUT'=>'About',
 	'MENU_HONOR'=>'Honor',
 	'MENU_PARTNERS'=>'Partners',
 	'MENU_CONTACT'=>'Contact',
+	
+	'CONTACT_US'=>'Contact Us',
+	'CONTACT_NAME'=>'Name',
+	'CONTACT_NAME_VALUE'=>'永聯製造廠',
+	'CONTACT_ADDRESS'=>'Address',
+	'CONTACT_ADDRESS_VALUE'=>'廣東省東莞市茶山鎮南社工業區立本工業園China',
+	'CONTACT_ZIP_CODE'=>'Zip Code',
+	'CONTACT_TEL'=>'Tel',
+	'CONTACT_FAX'=>'Fax',
+	'CONTACT_WEBSITE'=>'Website',
+	'CONTACT_PERSON'=>'Contacts',
+	'CONTACT_PERSON_VALUE'=>'朱瑩瑩',
+	'CONTACT_EMAIL'=>'Email',
+	'CONTACT_SHOP'=>'Shop',
+);
 );
--- Application/Home/Lang/zh-cn.php
+++ Application/Home/Lang/zh-cn.php
@@ -6,4 +6,18 @@ return array(
 	'MENU_HONOR'=>'公司荣誉',
 	'MENU_PARTNERS'=>'合作伙伴',
 	'MENU_CONTACT'=>'联系我们',
+	
+	'CONTACT_US'=>'联系我们',
+	'CONTACT_NAME'=>'名称',
+	'CONTACT_NAME_VALUE'=>'永联织造厂',
+	'CONTACT_ADDRESS'=>'地址',
+	'CONTACT_ADDRESS_VALUE'=>'广东省东莞市茶山镇南社工业区立本工业园',
+	'CONTACT_ZIP_CODE'=>'邮编',
+	'CONTACT_TEL'=>'总机',
+	'CONTACT_FAX'=>'传真',
+	'CONTACT_WEBSITE'=>'网址',
+	'CONTACT_PERSON'=>'联系人',
+	'CONTACT_PERSON_VALUE'=>'朱莹莹',
+	'CONTACT_EMAIL'=>'邮箱',
+	'CONTACT_SHOP'=>'商城',
 );
