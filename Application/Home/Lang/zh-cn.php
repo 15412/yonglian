25fd0557229903eee774f5b823ecdfbb09c141e1
disdao <sunrain992@hotmail.com>
2018-01-09 23:09:18 +0800

    公司簡介界面桌面端和移動端

--- Application/Home/Lang/zh-cn.php
+++ Application/Home/Lang/zh-cn.php
@@ -20,4 +20,17 @@ return array(
 	'CONTACT_PERSON_VALUE'=>'朱莹莹',
 	'CONTACT_EMAIL'=>'邮箱',
 	'CONTACT_SHOP'=>'商城',
+
+	'ABOUT_CONTEXT'=>'永联织造是世界前三的童装生产商，成立于1972年在香港成立，由阮氏家族和（王）氏家族 拥有公司100%的股权。1986年在大陆先后于东莞和广东韶关开设永联织造厂，拥有32间车间， 总员工数超过10000人，主要生产婴儿及儿童服装，年产量约1.2亿件。公司拥有超过40年的生
+	产和出口经营优势，产品出口到美国，英国，欧洲，加拿大，澳大利亚和亚洲各地，并获得了 Carter’s 、Tesco、Nike、Target、Walmart等公司的指定生产商。公司以专注生产婴儿、 儿童服装为核心，注重关爱儿童健康成长，成为儿童服装市场的前驱和专家。',
+	'ABOUT_LEFT1_IMG_DESC' => '左一',
+	'ABOUT_LEFT2_IMG_DESC' => '左二',
+	'ABOUT_LEFT3_IMG_DESC' => '左三',
+	'ABOUT_LEFT4_IMG_DESC' => '左四',
+	'ABOUT_MID1_IMG_DESC' => '中一',
+	'ABOUT_MID2_IMG_DESC' => '中二',
+	'ABOUT_RIGHT1_IMG_DESC' => '右一',	
+	'ABOUT_RIGHT2_IMG_DESC' => '右二',	
+	'ABOUT_RIGHT3_IMG_DESC' => '右三',	
+	'ABOUT_RIGHT4_IMG_DESC' => '右四',	
 );
--- Application/Home/Lang/zh-tw.php
+++ Application/Home/Lang/zh-tw.php
@@ -20,4 +20,18 @@ return array(
 	'CONTACT_PERSON_VALUE'=>'朱瑩瑩',
 	'CONTACT_EMAIL'=>'郵箱',
 	'CONTACT_SHOP'=>'商城',
+
+	'ABOUT_CONTEXT'=>'永聯織造是世界前三的童裝生產商，成立於1972年在香港成立，由阮氏家族和（王）氏家族擁有公司100%的股權。1986年在大陸先後於東莞和廣東韶關開設永聯織造廠，擁有32間車間，總員工數超過10000人，主要生產嬰兒及兒童服裝，年產量約1.2億件。公司擁有超過40年的生
+	產和出口經營優勢，產品出口到美國，英國，歐洲，加拿大，澳大利亞和亞洲各地，並獲得了Carter’s、Tesco、Nike、Target、Walmart等公司的指定生產商。公司以專注生產嬰兒、兒童服裝為覈心，注重關愛兒童健康成長，成為兒童服裝市場的前驅和專家。',
+	'ABOUT_LEFT1_IMG_DESC' => '左一',
+	'ABOUT_LEFT2_IMG_DESC' => '左二',
+	'ABOUT_LEFT3_IMG_DESC' => '左三',
+	'ABOUT_LEFT4_IMG_DESC' => '左四',
+	'ABOUT_MID1_IMG_DESC' => '中一',
+	'ABOUT_MID2_IMG_DESC' => '中二',
+	'ABOUT_RIGHT1_IMG_DESC' => '右一',	
+	'ABOUT_RIGHT2_IMG_DESC' => '右二',	
+	'ABOUT_RIGHT3_IMG_DESC' => '右三',	
+	'ABOUT_RIGHT4_IMG_DESC' => '右四',	
+
 );
