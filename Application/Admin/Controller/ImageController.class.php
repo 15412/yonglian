fc49693df651c9b45cc41ae3e474406cb92d2d91
rohochan <rohochan@users.noreply.github.com>
2018-01-21 22:26:42 +0800

    调整头部 调整联系我们 套图片数据
    Signed-off-by: rohochan <rohochan@users.noreply.github.com>

--- Application/Admin/Controller/ImageController.class.php
+++ Application/Admin/Controller/ImageController.class.php
@@ -14,28 +14,28 @@ namespace Admin\Controller;
  * @author rohochan <rohochan@gamil.com>
  */
 class ImageController extends ThinkController {
-	
 	private $model = 'image'; /*在OneThink模型管理中查看自己模型标识（不是名称）修改此处*/
 	
-	public function index(){
-		return $this->lists($this->model);
+	public function index($model = null, $p = 0){
+		return $this->lists($this->model, $p);
 	}
 	
-	public function lists( $model = null ){
-		return 	parent::lists($this->model);
+	public function lists($model = null, $p = 0){
+		return 	parent::lists($this->model, $p);
 	}
 	
-	public function add( $model = null ){
+	public function add($model = null ){
 		$model = M('Model')->getByName($this->model);
 		return 	parent::add( $model['id'] );
 	}
 	
-	public function edit( $model = null, $id = 0 ){
+	public function edit($model = null, $id = 0 ){
 		$id || $this->error('请选择要操作的数据！');
 		$model = M('Model')->getByName($this->model);
 		return parent::edit( $model['id'], $id );
 	}
-	public function del( $model = null, $ids=null ){
+	
+	public function del($model = null, $ids=null ){
 		$model = M('Model')->getByName($this->model);
 		return parent::del( $model['id'], $ids );
 	}
